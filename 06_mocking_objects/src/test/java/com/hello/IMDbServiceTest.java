adac275ca9c12b25aa1d6a0c1d329501a3284d16
harsh <harsh.singh@skillup.tech>
2025-02-25 21:09:29 +0530

    Test case reverted

--- 06_mocking_objects/src/test/java/com/hello/IMDbServiceTest.java
+++ 06_mocking_objects/src/test/java/com/hello/IMDbServiceTest.java
@@ -32,57 +32,5 @@ public class IMDbServiceTest {
 
    
 
-    @Test
-    void testSearchByTitle() {
-        // Mocking IMDbService
-        imdbService = mock(IMDbService.class);
-        // return the GOOD_SEARCH test fixture data.
-        when(imdbService.searchTitles("Babmi")).thenReturn(testResponses.get("GOOD_SEARCH"));
-        
-        JSONObject result = imdbService.searchTitles("Babmi");
-        assertNotNull(result);
-        assertEquals("Babmi", result.getJSONArray("results").getJSONObject(0).getString("title"));
-        assertEquals("tt1375666", result.getJSONArray("results").getJSONObject(0).getString("id"));
-    }
-
-
-    @Test
-void testSearchWithNoResults() {
-    // Mocking IMDbService
-    imdbService = mock(IMDbService.class);
-
-    // Returning an Empty JSONObject for "Bambi" (Fixing the typo)
-    when(imdbService.searchTitles("Bambi")).thenReturn(new JSONObject());
-
-    JSONObject result = imdbService.searchTitles("Bambi");
-
-    // Checking if the returned JSON object is empty
-    assertNotNull(result);  // Ensure result is not null
-    assertTrue(result.isEmpty());
-}
-
-
-    @Test
-    void testInvalidApiKey() {
-        imdbService = mock(IMDbService.class);
-        when(imdbService.searchTitles("Bambi")).thenReturn(testResponses.get("INVALID_API"));
-
-        JSONObject result = imdbService.searchTitles("Bambi");
-        
-        assertNotNull(result);
-        assertEquals("Invalid API Key", result.getString("errorMessage"));
-    }
-
-    @Test
-    void testMovieRatings() {
-        imdbService = mock(IMDbService.class);
-        when(imdbService.movieRatings("tt1375666")).thenReturn(testResponses.get("GOOD_RATING"));
-
-        JSONObject result = imdbService.movieRatings("tt1375666");
-        
-        assertNotNull(result);
-        assertEquals("Bambi", result.getString("title"));
-        assertEquals(3, result.getInt("filmAffinity"));
-        assertEquals(5, result.getInt("rottenTomatoes"));
-    }
+    
 }
